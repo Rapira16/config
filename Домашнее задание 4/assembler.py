7ad38d4aed55bd0bb118d42b660dddba868bbb12
Sukhanov Dmitry <79788948+Rapira16@users.noreply.github.com>
2024-11-21 15:33:51 +0300

    Update assembler.py
    модифицировал ассемблер

--- Домашнее задание 4/assembler.py
+++ Домашнее задание 4/assembler.py
@@ -15,13 +15,18 @@ def assemble(input_file, output_file, log_file):
         for line in f:
             parts = line.strip().split()
             cmd = parts[0]
-            A = int(parts[1])
-            B = int(parts[2])
-            C = int(parts[3])
+
             if cmd == 'LOAD':
+                A = int(parts[1])  # Индекс вектора
+                value = int(parts[2])  # Значение для загрузки
+                C = int(parts[3])  # Адрес для записи
                 binary_code.extend(struct.pack('>B B I', commands[cmd], A, C))
                 log.append(f"{line.strip()}=0x{binary_code[-6:].hex()}")
+
             elif cmd == 'LESS_THAN':
+                A = int(parts[1])  # Индекс вектора A
+                B = int(parts[2])  # Индекс вектора B
+                C = int(parts[3])  # Адрес для результата
                 binary_code.extend(struct.pack('>B B B', commands[cmd], A, B))
                 binary_code.append(C)  # C будет 1 байт (адрес для результата)
                 log.append(f"{line.strip()}=0x{binary_code[-5:].hex()}")
